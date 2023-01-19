4f3b493f39b0c1e34e87aa8abe5b320090c40d0f
Moisés Gamio Vergara <MGV@selly.biz>
2023-01-19 17:25:07 +0100

    initial commit

---
+++ src/main/java/dev/codersite/rateLimit/RateLimitApplication.java
@@ -0,0 +1,13 @@
+package dev.codersite.rateLimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/codersite/rateLimit/RateLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codersite.rateLimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
